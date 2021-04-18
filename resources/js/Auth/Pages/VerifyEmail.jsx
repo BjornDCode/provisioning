45d7889e3dcd3b5a833d85eba805f7eca0b8c3d9
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2021-04-18 14:14:40 +0200

    Style Verify email page

--- resources/js/Auth/Pages/VerifyEmail.jsx
+++ resources/js/Auth/Pages/VerifyEmail.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Inertia } from '@inertiajs/inertia'
 
-import Base from '@/Shared/Components/Layouts/Base'
+import Auth from '@/Auth/Components/Layouts/Auth'
 
 import Link from '@/Shared/Components/Leafs/Link'
 import Button from '@/Shared/Components/Leafs/Button'
@@ -21,24 +21,28 @@ const VerifyEmail = () => {
     }
 
     return (
-        <Base>
-            <h1>Verify email</h1>
-
-            <Paragraph>
+        <Auth title="Verify email">
+            <Paragraph className="mb-6">
                 Thanks for signing up! Before getting started, could you verify
                 your email address by clicking on the link we just emailed to
                 you? If you didn't receive the email, we will gladly send you
                 another.
             </Paragraph>
 
-            <Form onSubmit={onResend}>
-                <Button type="submit">Resend verification email</Button>
-            </Form>
+            <div className="flex space-x-6">
+                <Form onSubmit={onResend} className="w-1/2">
+                    <Button type="submit" size="large">
+                        Resend email
+                    </Button>
+                </Form>
 
-            <Form onSubmit={onLogout}>
-                <Button type="submit">Log out</Button>
-            </Form>
-        </Base>
+                <Form onSubmit={onLogout} className="w-1/2">
+                    <Button type="submit" size="large">
+                        Log out
+                    </Button>
+                </Form>
+            </div>
+        </Auth>
     )
 }
 
