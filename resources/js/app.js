5ebec067e3832d4989500525e0bbcf9136c324c4
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2021-05-11 22:27:48 +0200

    Inject websocket key as env variable

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ window.Pusher = Pusher
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '3263492761',
+    key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
